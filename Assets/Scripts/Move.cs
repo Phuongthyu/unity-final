2ffe65b4053fecbf8991ab0fd7a3d95aa82a1591
Phuongthyu <thuylpgcs200492@fpt.edu.vn>
2024-03-06 13:23:41 +0700

    ok

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public bool Jump;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float h_move = Input.GetAxis("Player1_Horizontal");
+
+        rb.velocity = new Vector2(h_move * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && Jump && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0); // Đặt vận tốc y về 0 trước khi nhảy
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Terrian")
+        {
+            Jump = true;
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Terrian")
+        {
+            Jump = false;
+            isGrounded = false;
+        }
+    }
+}
